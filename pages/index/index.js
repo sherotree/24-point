01bbc80d32a0b1af6f24a27693b8dbd3db0ad9c8
sherotree <wytreeshero@163.com>
2019-10-11 18:47:42 +0800

    work on 24 point

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    foo: 'xxx',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
